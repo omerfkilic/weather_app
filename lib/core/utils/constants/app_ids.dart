d1c96fdaa7871aef52a5f2cef2b84abc8410c4df
OmerFarukKilicIntime <omerfaruk.kilic@intimeinfo.com.tr>
2023-10-11 15:20:15 +0300

    fixed hidden api keys logic

--- lib/core/utils/constants/app_ids.dart
+++
@@ -1,9 +0,0 @@
-part of 'constants.dart';
-
-class _AppIds {
-  _AppIds._();
-
-  //ignored this file for security
-  //you can take this appid from : 'https://openweathermap.org/api'
-  String get openWeatherMapFreeAppId => '';
-}
--- lib/core/utils/constants/constants.dart
+++ lib/core/utils/constants/constants.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: library_private_types_in_public_api
 
 part 'asset_paths.dart';
-part 'app_ids.dart';
 
 ///Constants
 class CConsts {
@@ -9,8 +8,4 @@ class CConsts {
   static final _AssetPaths _assetPaths = _AssetPaths._();
 
   static _AssetPaths get assetPaths => _assetPaths;
-  
-  static final _AppIds _appIds = _AppIds._();
-
-  static _AppIds get appIds => _appIds;
 }
